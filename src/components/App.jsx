fb64e77eb5f0e0fe57ca3ead4f7359ced21385b4
IvanE <sportsmenq@mail.ru>
2023-03-03 20:18:25 +0500

    fix build_3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,16 @@ import Login from "./UI/Login"
 import SignUp from "./UI/SignUp"
 import Footer from "./UI/Footer"
 import NotFound from "./UI/NotFound"
-import Catalog from "./../containers/Catalog"
-import Tables from "/UI/catalog/Tables/Tables"
-import Chairs from "/UI/catalog/Chairs/Chairs"
-import Swing from "/UI/catalog/Swing/Swing"
-import Braziers from "/UI/catalog/Braziers/Braziers"
-import Other from "/UI/catalog/Other/Other"
-import Gallery from "/UI/Gallery"
-import Contacts from "./UI/Contacts"
-import Payment from "./UI/Payment"
-import Profile from "./UI/Profile"
+// import Catalog from "./../containers/Catalog"
+// import Tables from "./UI/catalog/Tables/Tables"
+// import Chairs from "./UI/catalog/Chairs/Chairs"
+// import Swing from "./UI/catalog/Swing/Swing"
+// import Braziers from "./UI/catalog/Braziers/Braziers"
+// import Other from "./UI/catalog/Other/Other"
+// import Gallery from "./UI/Gallery"
+// import Contacts from "./UI/Contacts"
+// import Payment from "./UI/Payment"
+// import Profile from "./UI/Profile"
 import Cart from "./../containers/Cart"
 import axios from 'axios'
 
@@ -54,42 +54,42 @@ class App extends Component {
                     <Route exact path="/" element={<Main/>}/>
                     <Route exact path="/login" element={<Login/>}/>
                     <Route exact path="/sign_up" element={<SignUp/>}/>
-                    <Route exact path="/catalog"
-                           element={<Catalog tables={tables}
-                                                  isReady={isReady}
-                                                  chairs={chairs}
-                                                  isReadyChairs={isReadyChairs}
-                                                  swing={swing}
-                                                  isReadySwing={isReadySwing}
-                                                  braziers={braziers}
-                                                  isReadyBraziers={isReadyBraziers}
-                                                  other={other}
-                                                  isReadyOther={isReadyOther}/>}
-                    />
-                    <Route exact path="/catalog/tables"
-                           element={<Tables tables={tables}
-                                            isReady={isReady}/>}
-                    />
-                    <Route exact path="/catalog/chairs"
-                           element={<Chairs chairs={chairs}
-                                                 isReadyChairs={isReadyChairs}/>}
-                    />
-                    <Route exact path="/catalog/swing"
-                           element={<Swing swing={swing}
-                                                isReadySwing={isReadySwing}/>}
-                    />
-                    <Route exact path="/catalog/braziers"
-                           element={<Braziers braziers={braziers}
-                                                   isReadyBraziers={isReadyBraziers}/>}
-                    />
-                    <Route exact path="/catalog/other"
-                           element={<Other other={other}
-                                                isReadyOther={isReadyOther}/>}
-                    />
-                    <Route exact path="/gallery" element={<Gallery/>}/>
-                    <Route exact path="/contacts" element={<Contacts/>}/>
-                    <Route exact path="/payment" element={<Payment/>}/>
-                    <Route exact path="/profile" element={<Profile/>}/>
+                    {/*<Route exact path="/catalog"*/}
+                    {/*       element={<Catalog tables={tables}*/}
+                    {/*                              isReady={isReady}*/}
+                    {/*                              chairs={chairs}*/}
+                    {/*                              isReadyChairs={isReadyChairs}*/}
+                    {/*                              swing={swing}*/}
+                    {/*                              isReadySwing={isReadySwing}*/}
+                    {/*                              braziers={braziers}*/}
+                    {/*                              isReadyBraziers={isReadyBraziers}*/}
+                    {/*                              other={other}*/}
+                    {/*                              isReadyOther={isReadyOther}/>}*/}
+                    {/*/>*/}
+                    {/*<Route exact path="/catalog/tables"*/}
+                    {/*       element={<Tables tables={tables}*/}
+                    {/*                        isReady={isReady}/>}*/}
+                    {/*/>*/}
+                    {/*<Route exact path="/catalog/chairs"*/}
+                    {/*       element={<Chairs chairs={chairs}*/}
+                    {/*                             isReadyChairs={isReadyChairs}/>}*/}
+                    {/*/>*/}
+                    {/*<Route exact path="/catalog/swing"*/}
+                    {/*       element={<Swing swing={swing}*/}
+                    {/*                            isReadySwing={isReadySwing}/>}*/}
+                    {/*/>*/}
+                    {/*<Route exact path="/catalog/braziers"*/}
+                    {/*       element={<Braziers braziers={braziers}*/}
+                    {/*                               isReadyBraziers={isReadyBraziers}/>}*/}
+                    {/*/>*/}
+                    {/*<Route exact path="/catalog/other"*/}
+                    {/*       element={<Other other={other}*/}
+                    {/*                            isReadyOther={isReadyOther}/>}*/}
+                    {/*/>*/}
+                    {/*<Route exact path="/gallery" element={<Gallery/>}/>*/}
+                    {/*<Route exact path="/contacts" element={<Contacts/>}/>*/}
+                    {/*<Route exact path="/payment" element={<Payment/>}/>*/}
+                    {/*<Route exact path="/profile" element={<Profile/>}/>*/}
                     <Route exact path="/cart" element={<Cart/>}/>
                     <Route path="*" element={<NotFound />} />
                 </Routes>
